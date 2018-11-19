6deed016d8d5f7958bec8a0c23128d78c68ff0e0
Rene Milzarek <rene@crashtest-security.com>
2018-11-19 08:33:01 +0100

    feat: configure the Google Cloud project for the build and deployment of the vulnerable application

---
+++ infrastructure/variables.tf
@@ -0,0 +1,28 @@
+/*
+ * This file is part of the DevSecOps example.
+ *
+ * Copyright 2018 Crashtest Security GmbH <dev@crashtest-security.com>
+ *
+ * Author:  René Milzarek <rene@crashtest-security.com>
+ *
+ * All rights reserved.
+ */
+
+
+variable "google_project_id" {
+  type        = "string"
+  description = "Google Cloud Platform project identifier."
+  default     = "cs-devsecops-example"
+}
+
+variable "google_project_number" {
+  type        = "string"
+  description = "Google Cloud Platform project number (numeric)."
+}
+
+variable "google_region" {
+  type        = "string"
+  description = "Google Cloud Platform region."
+  default     = "europe-west3" # Germany - Frankfurt Region
+}
+
