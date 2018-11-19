564352b297dcffbf68b9b6d676a15456728acd35
Janosch Maier <janosch@crashtest-security.com>
2018-11-19 16:32:48 +0100

    feat: disable CSRF for request

--- application/vulnerable/views.py
+++ application/vulnerable/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
-
+@csrf_exempt
 def index(request):
     context = {"name": request.POST.get("name")}
     return render(request, "index.html", context)
